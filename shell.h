c29a04082f870567a09d7fe67637e707ebc3b6c8
Muncherxyz <5532@holbertonstudents.com>
2023-04-11 11:45:38 -0700

    builtinfix

--- shell.h
+++ shell.h
@@ -12,8 +12,12 @@
 #define DELIM "\t\n"
 
 
+int main(int argc, char **argv);
 
 void execmd(char **argv);
 char *get_location(char *command);
+/* use your own _str files*/
 
+void builtfunc(char **buf, char **args, char **env, int *exstat);
+/* add _putchar and _puts */
 #endif
