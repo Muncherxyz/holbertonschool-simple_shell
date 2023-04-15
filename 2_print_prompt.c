7828fb83ac272c514bf612db9513cecfd4287a57
Dom <you@example.com>
2023-04-15 12:22:46 -0700

    broke down main.c into smaller files

---
+++ 2_print_prompt.c
@@ -0,0 +1,11 @@
+#include "shell.h"
+/**
+* print_prompt - this function prints the prompt
+* @prompt: this is a pointer
+* Return: nothing
+*/
+
+void print_prompt(char *prompt) 
+{
+    printf("%s", prompt);
+}
---
+++ 2_shell.c
@@ -0,0 +1,35 @@
+#include "shell.h"
+
+/**
+* main - the entry point of the program
+* @ac: integer that represents the num of command-line arguments
+* @argv: pointer to an array of strings
+* Return: 0
+*/
+int main(int ac, char **argv)
+{
+	char *prompt = "$ ", *lineptr = NULL, **cmd_argv;
+	size_t n = 0;
+	const char *delim = " \n";
+	int num_of_tokens;
+
+	(void)ac;
+
+	while (1)
+	{
+		print_prompt(prompt);
+		if (read_input(&lineptr, &n) == -9)
+			break;
+		num_of_tokens = tokenize_input(lineptr, delim, &cmd_argv);
+		if (num_of_tokens == -9)
+			break;
+		execute_command(cmd_argv);
+		for (int i = 0; i < num_of_tokens - 1; i++)
+			free(cmd_argv[i]);
+		free(cmd_argv);
+		free(lineptr);
+		n = 0;
+	}
+
+	return (0);
+}
