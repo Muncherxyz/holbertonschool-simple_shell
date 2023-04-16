c298ad6f2a7c2d63a5fa378865896b26bc8f4569
Muncherxyz <5532@holbertonstudents.com>
2023-04-15 17:25:58 -0700

    update strcmp to _strcmp

--- builtins.c
+++ builtins.c
@@ -2,7 +2,7 @@
 /**
  * builtfunc - function that implements the exit builtin
  * @buf: buffer
- * @args: the parsed arguments
+ * @args: the parsed arguments.
  * @env: the environment
  * @exstat: exit status
  * Return: void
