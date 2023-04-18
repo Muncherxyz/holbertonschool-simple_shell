66b8be4a3b678812b2e33b01aece3bd0b0b5d934
Dom <you@example.com>
2023-04-17 21:21:21 -0700

    betty fix in tokenizer file

--- tokenizer.c
+++ tokenizer.c
@@ -4,14 +4,15 @@
 * @lineptr: a pointer to the address of a character
 * @delim: a pointer to the address of a delimeter
 * @argv: a pointer to a pointer to a pointer to a character
-* Return: number of tokens 
+* Return: number of tokens
 */
-int tokenize_input(char *lineptr, const char *delim, char ***argv) 
+int tokenize_input(char *lineptr, const char *delim, char ***argv)
 {
 	int num_of_tokens = 0;
 
 	char *token = strtok(lineptr, delim);
-	while (token != NULL) 
+
+	while (token != NULL)
 	{
 		num_of_tokens++;
 		token = strtok(NULL, delim);
@@ -19,13 +20,14 @@ int tokenize_input(char *lineptr, const char *delim, char ***argv)
 	num_of_tokens++;
 
 	*argv = malloc(sizeof(char *) * num_of_tokens);
-	if (*argv == NULL) 
+	if (*argv == NULL)
 	{
 		perror("Memory allocation error");
-		return -9;
+		return (-9);
 	}
 
 	char *saveptr = NULL;
+
 	token = strtok_r(lineptr, delim, &saveptr);
 	for (int i = 0; i < num_of_tokens - 1; i++)
 	{
@@ -34,5 +36,5 @@ int tokenize_input(char *lineptr, const char *delim, char ***argv)
 	}
 	(*argv)[num_of_tokens - 1] = NULL;
 
-	return num_of_tokens;
+	return (num_of_tokens);
 }
