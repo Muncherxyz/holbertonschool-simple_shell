b3e60524bec0043aac57de5382b583cf348aff88
Dom <you@example.com>
2023-04-17 20:44:02 -0700

    betty fix

--- strings_2.c
+++ strings_2.c
@@ -11,4 +11,13 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] != '\0';     j++)192                 {193                         if (haystack[i + j] !=     needle[j])194                                 break;195                 }196                 if (needle[j] == '\0')197                         return (haystack + i); 198         }199         return (NULL);200 }
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+			if (needle[j] == '\0')
+				return (haystack + i);
+		}
+	}
+	return (NULL);
+}
