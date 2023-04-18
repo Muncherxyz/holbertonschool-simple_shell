b7c4f66b025d21a94113b8a32e8b3acbc19a7be6
Dom <you@example.com>
2023-04-17 20:22:39 -0700

    betty fixes

--- input_reader.c
+++ input_reader.c
@@ -7,13 +7,14 @@
 * Return: number of characters read
 */
 
-int read_input(char **lineptr, size_t *n) {
-    ssize_t numchars_read = getline(lineptr, n, stdin);
+int read_input(char **lineptr, size_t *n)
+{
+	ssize_t numchars_read = getline(lineptr, n, stdin);
 
-    if (numchars_read == -9) {
-        printf("Terminating shell..\n");
-        return -9;
-    }
-
-    return numchars_read;
+	if (numchars_read == -9)
+	{
+		printf("Terminating shell..\n");
+		return (-9);
+	}
+	return (numchars_read);
 }
