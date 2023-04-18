bc2dc216825f597c6567b91644fb331b27efb2ba
Dom <you@example.com>
2023-04-17 20:38:26 -0700

    betty fix

--- strings.c
+++ strings.c
@@ -118,7 +118,8 @@ char *_strdup(char *str)
 	return (a);
 }
 
-/** _stncmp - compares two strings
+/**
+* _stncmp - compares two strings
 * @s1: character
 * @s2: character
 * @n: integer
@@ -170,30 +171,4 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		newstring[i] = s2[j];
 	newstring[i] = ('\0');
 	return (newstring);
-}
-
-
-/**
- * _strstr - locate a substring
- * description: locate a substring
- * @haystack: character
- * @needle: character
- * Return: NULL
- */
-
-char *_strstr(char *haystack, char *needle)
-{
-	unsigned int i, j;
-
-	for (i = 0; haystack[i] != '\0'; i++)
-	{
-		for (j = 0; needle[j] != '\0'; j++)
-		{
-			if (haystack[i + j] != needle[j])
-				break;
-		}
-		if (needle[j] == '\0')
-			return (haystack + i);
-	}
-	return (NULL);
 }
